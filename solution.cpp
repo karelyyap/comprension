29480ce066f6c2ae53c09118d1e19ac90f6ae8c2
karelyyap <karely2005@gmail.com>
2026-06-27 14:38:46 -0700

    Fix compress and decompress to match problem specification exactly

--- solution.cpp
+++ solution.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <iomanip>
-#include <unordered_map>
+#include <map>
 #include <set>
 
 using namespace std;
@@ -12,70 +12,32 @@ int main() {
     cin.tie(NULL);
 
     string modo;
-    if (!(cin >> modo)) return 0;
+    cin >> modo;
 
     if (modo == "compress") {
         int n, m;
         cin >> n >> m;
-        int total_pixeles = n * m;
+        int total = n * m;
         
-        vector<unsigned int> img;
-        img.reserve(total_pixeles);
+        vector<unsigned int> pixeles;
         set<unsigned int> colores_unicos;
         
-        for (int i = 0; i < total_pixeles; i++) {
-            string hex_str;
-            cin >> hex_str;
-            unsigned int color = stoul(hex_str, nullptr, 16);
-            img.push_back(color);
+        for (int i = 0; i < total; i++) {
+            string hex;
+            cin >> hex;
+            unsigned int color = stoul(hex, nullptr, 16);
+            pixeles.push_back(color);
             colores_unicos.insert(color);
         }
         
-        vector<unsigned int> diccionario_base(colores_unicos.begin(), colores_unicos.end());
-        unordered_map<unsigned int, int> mapa_base;
-        for (size_t i = 0; i < diccionario_base.size(); i++) {
-            mapa_base[diccionario_base[i]] = i;
-        }
+        // Crear diccionario de colores únicos
+        vector<unsigned int> diccionario(colores_unicos.begin(), colores_unicos.end());
         
-        // LZW compression
-        unordered_map<long long, int> dict_lzw;
-        int proximo_codigo = diccionario_base.size();
-        vector<int> codigos_comprimidos;
-        codigos_comprimidos.reserve(total_pixeles);
-        
-        const int MAX_DICT = 65536;
-        
-        if (total_pixeles > 0) {
-            int codigo_actual = mapa_base[img[0]];
-            
-            for (int i = 1; i < total_pixeles; i++) {
-                unsigned int color = img[i];
-                long long clave = ((long long)codigo_actual << 32) | color;
-                
-                if (dict_lzw.find(clave) != dict_lzw.end()) {
-                    codigo_actual = dict_lzw[clave];
-                } else {
-                    codigos_comprimidos.push_back(codigo_actual);
-                    
-                    if (proximo_codigo < MAX_DICT) {
-                        dict_lzw[clave] = proximo_codigo++;
-                    }
-                    
-                    codigo_actual = mapa_base[color];
-                }
-            }
-            codigos_comprimidos.push_back(codigo_actual);
-        }
-        
-        // Output
-        cout << (1 + diccionario_base.size() + 1 + codigos_comprimidos.size()) << "\n";
-        cout << diccionario_base.size();
-        for (unsigned int color : diccionario_base) {
-            cout << " " << color;
-        }
-        cout << " " << codigos_comprimidos.size();
-        for (int codigo : codigos_comprimidos) {
-            cout << " " << codigo;
+        // Output: solo dos líneas
+        cout << diccionario.size() << "\n";
+        for (size_t i = 0; i < diccionario.size(); i++) {
+            cout << diccionario[i];
+            if (i < diccionario.size() - 1) cout << " ";
         }
         cout << "\n";
         
@@ -83,67 +45,25 @@ int main() {
         int n, m, k;
         cin >> n >> m >> k;
         
-        int num_base;
-        cin >> num_base;
-        vector<unsigned int> diccionario_base(num_base);
-        for (int i = 0; i < num_base; i++) {
-            cin >> diccionario_base[i];
+        vector<unsigned int> diccionario(k);
+        for (int i = 0; i < k; i++) {
+            cin >> diccionario[i];
         }
         
-        int num_codigos;
-        cin >> num_codigos;
-        vector<int> codigos(num_codigos);
-        for (int i = 0; i < num_codigos; i++) {
-            cin >> codigos[i];
+        vector<int> indices(n * m);
+        for (int i = 0; i < n * m; i++) {
+            cin >> indices[i];
         }
         
-        // Decompression dictionary
-        vector<vector<unsigned int>> dict_lzw;
-        dict_lzw.reserve(65536);
-        
-        for (int i = 0; i < num_base; i++) {
-            dict_lzw.push_back({diccionario_base[i]});
-        }
-        
-        int pixeles_impresos = 0;
-        
-        auto imprimir_pixel = [&](unsigned int color) {
-            cout << hex << uppercase << setfill('0') << setw(8) << color;
-            pixeles_impresos++;
-            if (pixeles_impresos % m == 0) cout << "\n";
-            else cout << " ";
-        };
-        
-        if (num_codigos > 0) {
-            int codigo_anterior = codigos[0];
-            for (unsigned int color : dict_lzw[codigo_anterior]) {
-                imprimir_pixel(color);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int idx = i * m + j;
+                cout << hex << uppercase << setfill('0') << setw(8) << diccionario[indices[idx]];
+                if (j < m - 1) cout << " ";
             }
-            
-            for (int i = 1; i < num_codigos; i++) {
-                int codigo_actual = codigos[i];
-                vector<unsigned int> entrada;
-                
-                if (codigo_actual < (int)dict_lzw.size()) {
-                    entrada = dict_lzw[codigo_actual];
-                } else if (codigo_actual == (int)dict_lzw.size()) {
-                    entrada = dict_lzw[codigo_anterior];
-                    entrada.push_back(dict_lzw[codigo_anterior][0]);
-                }
-                
-                for (unsigned int color : entrada) {
-                    imprimir_pixel(color);
-                }
-                
-                if (dict_lzw.size() < 65536) {
-                    vector<unsigned int> nueva_entrada = dict_lzw[codigo_anterior];
-                    nueva_entrada.push_back(entrada[0]);
-                    dict_lzw.push_back(nueva_entrada);
-                }
-                
-                codigo_anterior = codigo_actual;
-            }
+            cout << "\n";
         }
     }
+    
     return 0;
 }
